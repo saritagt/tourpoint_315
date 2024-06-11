b7c4bf90f0384eecab3b7d6942b49e3637e0cb94
saritagt <158017344+saritagt@users.noreply.github.com>
2024-06-11 15:47:53 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ดอยอินทนนท์",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.48691849999824,
+            18.59077751402704
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูทับเบิก",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.48693995766666,
+            18.590838528032876
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะล้าน",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.77751099874888,
+            12.915868778398917
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะหลีเป๊ะ",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.29999744745373,
+            6.490014629194732
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะกูด",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            102.53488361544714,
+            11.65142966859382
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ถ้ำเชียงดาว",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.92943940400932,
+            19.394227758890594
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดร่องขุ่น",
+          "type": "catutal",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.46104613376798,
+            19.11502892511878
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขาค้อ",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.00289175007902,
+            16.641270349351274
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขาคิชฌกูฏ",
+          "type": "catutal",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            102.14454265086539,
+            12.804524210937956
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะเต่า",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.83799790530844,
+            10.090001060184761
+          ],
+          "type": "Point"
+        },
+        "id": 9
+      }
+    ]
+  }
